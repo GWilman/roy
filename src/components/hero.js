ec180fbb73f7d90a69eadb20b09ca9ded6be3633
GWilman <georgewilman1@gmail.com>
2018-10-08 22:03:00 +0100

    home page improvements

--- src/components/hero.js
+++ src/components/hero.js
@@ -7,6 +7,28 @@ class Hero extends Component {
       <section className="hero">
         <div className="hero-left"></div>
         <div className="hero-right"></div>
+        <div className="hero-body center-everything">
+          <div className="hero-content-left center-everything flex-column">
+            <h2 className="title is-2 has-text-white">For Businesses</h2>
+            <div className="card has-rounded-corners">
+              <div class="card-content">
+                <p class="text is-6">
+                  Find a lawyer and get a quote in minutes.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="hero-content-right center-everything flex-column">
+            <h2 className="title is-2 has-text-primary">For Lawyers</h2>
+            <div className="card has-rounded-corners">
+              <div class="card-content">
+                <p class="text is-6">
+                  Get hired for jobs that suit your skills and availability.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
         {/* <div className="hero-body center-everything flex-column">
           <div className="bg-logo"></div>
           <p className="title is-1 has-text-primary margin-double--bottom has-text-centered">
